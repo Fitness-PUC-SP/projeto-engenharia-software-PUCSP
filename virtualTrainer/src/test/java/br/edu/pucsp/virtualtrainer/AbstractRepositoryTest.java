625bfbca0ce0e2b337700e819e18b933d938482e
Glauco de Castilho <castilho.glauco@gmail.com>
2021-03-25 01:42:01 -0300

    Classe de config do swagger movida para pasta config Adição de classe abstrata de teste para repositorio e controller

--- virtualTrainer/src/test/java/br/edu/pucsp/virtualtrainer/AbstractRepositoryTest.java
+++
@@ -1,8 +0,0 @@
-package br.edu.pucsp.virtualtrainer;
-
-import org.springframework.test.annotation.Rollback;
-import org.springframework.test.context.junit4.AbstractTransactionalJUnit4SpringContextTests;
-
-@Rollback
-public class AbstractRepositoryTest extends AbstractTransactionalJUnit4SpringContextTests {
-}
