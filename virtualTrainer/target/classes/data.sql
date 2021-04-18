f17253a13116be7c00b86867fcfda88cb5c13f2a
Emily Durães <52414179+emilyduraes@users.noreply.github.com>
2021-04-18 00:47:48 -0300

    Delete virtualTrainer/target directory

--- virtualTrainer/target/classes/data.sql
+++
@@ -1,9 +0,0 @@
-------------------------- DML file for im memory database ---------------
---INSERT INTO Category (name) VALUES ('Bem Estar');
---INSERT INTO Category (name) VALUES ('Atividade Física');
---INSERT INTO Category (name) VALUES ('Educação Alimentar');
-
---INSERT INTO Field ( name, certificate, category) VALUES ( 'Nutricao', false, 3);
---INSERT INTO Field ( name, certificate, category) VALUES ( 'Yoga', false, 1);
---INSERT INTO Field ( name, certificate, category) VALUES ( 'Treino', true, 2);
-
